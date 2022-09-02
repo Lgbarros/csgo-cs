81128630c9b702c8b39fdbe79560c9ac136f7a7b
Lucas Barros <lucasg.barros.94@gmail.com>
2022-09-01 22:43:47 -0300

    Fixed mobile layout and desktop, change the style of h1,h2 and texts

--- assets/js/js.js
+++ assets/js/js.js
@@ -6,6 +6,7 @@ function switchTheme() {
 
     const theme = document.body.classList[0];
 
+    if (document.body.classList.contains('cs')) {
     document.getElementById("invert-text").innerHTML = "O jogo Counter-Strike nasceu de um simples mod, sem a pretensão de se tornar um grande sucesso, mas ao permitir que equipes de jogadores (ou clãs) entrassem em disputa, conseguiu alcançar um destaque que nem seus produtores poderiam prever.";
 
     document.getElementById("caminho-de-rato").src="./assets/img/cs/D4-4.png";
@@ -19,4 +20,19 @@ function switchTheme() {
     document.getElementById("ponte-t-inferno").src="./assets/img/cs/Inferno-1.png";
     document.getElementById("bomb-b-inferno").src="./assets/img/cs/Inferno-4.png";
     document.getElementById("base-t-inferno").src="./assets/img/cs/Inferno-5.png";
+    } else { 
+      document.getElementById("invert-text").innerHTML = "O Counter-Strike: Global Offensive (CS:GO) é a continuação do jogo de equipas cheio de ação que foi pioneiro quando foi lançado há 12 anos atrás. O CS: GO apresenta-se com novos mapas, personagens, armas e ainda oferece versões atualizadas de conteúdo do CS clássico.";
+
+    document.getElementById("caminho-de-rato").src="./assets/img/csgo/d2-img1.png";
+    document.getElementById("caixa-gorda").src="./assets/img/csgo/d2-img2.png";
+    document.getElementById("base-ct").src="./assets/img/csgo/destaque-d2.png";
+    document.getElementById("base-t").src="./assets/img/csgo/d2-img3.png";
+    document.getElementById("bomb-b").src="./assets/img/csgo/d2-img4.png";
+
+    document.getElementById("areia-inferno").src="./assets/img/csgo/inferno-img1.png";
+    document.getElementById("base-ct-inferno").src="./assets/img/csgo/inferno-img4.png";
+    document.getElementById("ponte-t-inferno").src="./assets/img/csgo/inferno-img3.png";
+    document.getElementById("bomb-b-inferno").src="./assets/img/csgo/inferno-img2.png";
+    document.getElementById("base-t-inferno").src="./assets/img/csgo/destaque-inferno.png";
+     }
   }
